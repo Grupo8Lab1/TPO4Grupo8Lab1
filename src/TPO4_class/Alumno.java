3ea4de546409410c1c5797dfa88e3ed3fb3e88c1
Ni-luteno <ni.luteno@gmail.com>
2022-09-30 16:23:53 -0300

    Initial commit

---
+++ src/TPO4_class/Alumno.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TPO4_class;
+
+/**
+ *
+ * @author Santi
+ */
+public class Alumno {
+    private int legajo;
+    private String apellido;
+    private String nombre;
+
+    public Alumno() {
+    }
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "legajo=" + legajo + ", apellido=" + apellido + ", nombre=" + nombre + '}';
+    }
+    
+    
+    
+}
---
+++ src/TPO4_class/TPO4Grupo8Lab1.java
@@ -0,0 +1,13 @@
+package TPO4_class;
+
+import Vistas.FormularioDeAlumnos;
+
+public class TPO4Grupo8Lab1 {
+
+    public static void main(String[] args) {
+        FormularioDeAlumnos form = new FormularioDeAlumnos();
+        form.setVisible(true);
+        
+    }
+    
+}
