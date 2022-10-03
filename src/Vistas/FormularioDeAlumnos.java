550d03ea7f404a94e08fb1c4b9d70f49de72161f
SantiagosWorkplace <santiagolucero00@gmail.com>
2022-10-03 00:36:25 -0300

    Intentos que no fueron intentados por lo que no lo intente

--- src/Vistas/FormularioDeAlumnos.java
+++ src/Vistas/FormularioDeAlumnos.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Vistas;
 
 import TPO4_class.Alumno;
 import java.awt.BorderLayout;
 import static TPO4_class.TPO4Grupo8Lab1.listaAlumnos;
-import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 /**
@@ -310,9 +305,6 @@ public class FormularioDeAlumnos extends javax.swing.JFrame {
         presentarvistas(a);
     }//GEN-LAST:event_jbFormularioDeInscripcionActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
